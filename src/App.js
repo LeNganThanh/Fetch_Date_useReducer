46a0763c43a24eaef880bd73c252008b32601817
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-13 13:27:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-
+import React, { useEffect, useReducer } from "react";
+import "./App.css";
+const initialState = {
+  resource: "Posts",
+  items: [],
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Posts": {
+      return {
+        ...state,
+        resource: action.value,
+      };
+    }
+    case "Users": {
+      return {
+        ...state,
+        resource: action.value,
+      };
+    }
+    case "Comments": {
+      return {
+        ...state,
+        resource: action.value,
+      };
+    }
+    case "data": {
+      return {
+        ...state,
+        items: action.value,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 function App() {
-  const [resource, setResource] = useState('Posts');
-  const [items, setItems] = useState([]);
-
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    // fetch(`https://jsonplaceholder.typicode.com/${resource}/?_limit=10`)
-    //   .then(response => response.json())
-    //   .then(json => setItems(json))
     async function fetchData() {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/${resource}?_limit=10`);
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/${state.resource}?_limit=10`
+      );
       const data = await response.json();
       setItems(data);
     }
     fetchData();
-  })
+  }, [state.resource]);
 
+  function setItems(val) {
+    dispatch({
+      type: "data",
+      value: val,
+    });
+  }
   return (
     <div className="container">
       <div>
-        <button onClick={() => setResource('Posts')}>Posts</button>
-        <button onClick={() => setResource('Users')}>Users</button>
-        <button onClick={() => setResource('Comments')}>Comments</button>
+        <button
+          onClick={() => {
+            dispatch({ type: "Posts", value: "Posts" });
+          }}
+        >
+          Posts
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "Users", value: "Users" });
+          }}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "Comments", value: "Comments" });
+          }}
+        >
+          Comments
+        </button>
       </div>
-      <h1>{resource}</h1>
+      <h1>{state.resource}</h1>
       <ul>
-        {items.map(item => <li key={item.id}>{JSON.stringify(item)}</li>)}
+        {state.items.map(item => (
+          <li key={item.id}>{JSON.stringify(item)}</li>
+        ))}
       </ul>
     </div>
   );
 }
-
 export default App;
